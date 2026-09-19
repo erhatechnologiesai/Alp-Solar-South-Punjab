cfe41befe0e0cf0e7669f4d8bcbb40ad03f41f7c
Syed Yasir Shahbaz <yasirshahbaz0843@gmail.com>
2026-09-19 19:46:19 +0500

    Add auto-redirect from vercel.app to alpsolarsp.com in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -36,6 +36,21 @@ const nextConfig: NextConfig = {
       },
     ];
   },
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "(.*)\\.vercel\\.app",
+          },
+        ],
+        destination: "https://alpsolarsp.com/:path*",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
